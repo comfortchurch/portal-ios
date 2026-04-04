cf56dc2463ee486438f3c44a44c98b67c747f502
Raja Simon <coderxsimon@gmail.com>
2026-04-04 13:07:35 +0000

    Add Firebase/FCM setup for push notifications

--- Pastor Benz/AppDelegate.swift
+++ Pastor Benz/AppDelegate.swift
@@ -5,15 +5,35 @@
 //  Created by Simon on 28/11/25.
 //
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        application.registerForRemoteNotifications()
         return true
     }
-}
 
-    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let token = fcmToken else { return }
+        print("📱 FCM Token: \(token)")
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMTokenDidUpdate"),
+            object: nil,
+            userInfo: ["token": token]
+        )
+    }
+}
